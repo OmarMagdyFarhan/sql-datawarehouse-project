75eb58a46eb780d1c02043ac024d85e9d4c06637
Omar Magdy <omarmagdygomaa22@gmail.com>
2026-01-13 16:36:44 +0200

    Initialize Database

---
+++ init_databaase.sql
@@ -0,0 +1,15 @@
+-- Create Database : Data Warehouse --
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+-- Create Schemas --
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
